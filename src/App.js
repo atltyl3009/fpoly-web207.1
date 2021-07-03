94e23b2cedd62133224caa852022d42a9a305a92
Tienanh <tienanhluu233574@gmail.com>
2021-07-03 08:25:21 +0700

    update2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Nav from "./components/nav";
+import Product from "./components/product";
+import "./dashboard.css";
+import data from "./data";
+export default function App() {
+  const products = data;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Header />
+        <div className="container-fluid">
+          <div className="row">
+            <Nav />
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              <Product products={products} />
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
